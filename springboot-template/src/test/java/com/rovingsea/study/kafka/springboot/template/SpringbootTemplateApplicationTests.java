3ec7306336d75d83c63e96cdd86b17d087bb1bff
= <=>
2022-06-26 16:29:27 +0800

    整合 springboot initialize

---
+++ springboot-template/src/test/java/com/rovingsea/study/kafka/springboot/template/SpringbootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rovingsea.study.kafka.springboot.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
